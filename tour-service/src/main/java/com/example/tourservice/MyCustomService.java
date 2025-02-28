e23c81778d812ed2f8a9dc50d5d548a242f59c08
Sadot Arefin <sadot.arefin@gmail.com>
2025-02-28 19:10:01 +0000

    created a new service

---
+++ tour-service/src/main/java/com/example/tourservice/MyCustomService.java
@@ -0,0 +1,11 @@
+package com.example.tourservice;
+
+import org.springframework.stereotype.Service;;
+
+
+@Service
+public class MyCustomService {
+  public MyCustomService() {
+    System.out.println("Called... 2");
+  }
+}
